525307ae7840c06b8ad6ca17071a75dff244f296
Steven Miyakawa SAM <samprofessional@gmail.com>
2015-03-07 15:25:12 -0800

    Add module to do sentiment analysis on text

---
+++ services/sentiment.js
@@ -0,0 +1,16 @@
+var request = require('request-promise');
+
+var sentiment = {
+  baseURL: 'https://api.idolondemand.com/1/api/sync/analyzesentiment/v1',
+  apiKey: process.env.HP_IDOL_API_KEY,
+  analyze: function(text) {
+    var url = this.baseURL + '?text=' + encodeURIComponent(text) + '&apiKey=' + this.apiKey;
+
+    return request.get(url)
+      .then(function(res) {
+        return { sentiment: JSON.parse(res) };
+      });
+  }
+};
+
+module.exports = sentiment;
