2c90756dc368c931eb453187c819f398c325d8c1
Devin Liu <devin.r.liu@gmail.com>
2015-03-07 14:11:22 -0800

    initial commit sorry

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var multer = require('multer');
 var app = express();
+var passport = require('passport');
+var FacebookStrategy = require('passport-facebook').Strategy
+
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -11,8 +14,61 @@ app.get('/', function(req, res) {
   res.json({ msg: 'Hello World!' });
 });
 
+app.get('/login', function(req, res) {
+  res.json({ msg: 'try again!' });
+});
+
+
 app.set('port', (process.env.PORT || 3000));
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'));
 });
 
+app.get('/auth/facebook',
+  passport.authenticate('facebook'));
+
+app.get('/auth/facebook/callback',
+  passport.authenticate('facebook', { failureRedirect: '/login', display: 'touch' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/');
+  });
+
+app.post('/auth/facebook/token',
+  passport.authenticate('facebook-token'),
+  function (req, res) {
+    // do something with req.user
+    res.send(req.user? 200 : 401);
+  }
+);
+
+app.get('/logout', function(req, res){
+  req.logout();
+  res.redirect('/');
+});
+
+passport.use(new FacebookStrategy({
+    clientID: process.env.FACEBOOK_APP_ID,
+    clientSecret: process.env.FACEBOOK_APP_SECRET,
+    callbackURL: "http://localhost:3000/auth/facebook/callback"
+  },
+  function(accessToken, refreshToken, profile, done) {
+    // User.findOrCreate({ facebookId: profile.id }, function(err, user) {
+    //   if (err) { return done(err); }
+    //   done(null, user);
+    // });
+    console.log(accessToken + " " + refreshToken + " " + profile)
+    if (done) {
+      return done(profile);
+    }
+  }
+));
+
+app.get('/auth/facebook', passport.authenticate('facebook'));
+
+// Facebook will redirect the user to this URL after approval.  Finish the
+// authentication process by attempting to obtain an access token.  If
+// access was granted, the user will be logged in.  Otherwise,
+// authentication has failed.
+app.get('/auth/facebook/callback',
+  passport.authenticate('facebook', { successRedirect: '/',failureRedirect: '/login' }));
