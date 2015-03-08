9bfae013adf3923c74c04a9a1db954874a53d4ce
Steven Miyakawa SAM <samprofessional@gmail.com>
2015-03-08 08:48:46 -0700

    Improve mongo logging

--- services/mongoLogger.js
+++ services/mongoLogger.js
@@ -1,7 +1,18 @@
 module.exports = function(err, result) {
   if(err) {
     console.error('insert failed:', err);
-  } else {
-    console.log("insert successful!");
+    return;
+  }
+
+  console.log('insert successful!');
+
+  var result = result[0];
+
+  if (result.text) {
+    console.log('text: ', result.text);
+  }
+  if (result.sentiment && result.sentiment.aggregate) {
+    console.log('sentiment: ' + result.sentiment.aggregate.sentiment);
+    console.log('sentiment score: ' + result.sentiment.aggregate.score);
   }
 };
