6a2cba09021c1457978dcd027fe8e64397987084
Steven Miyakawa SAM <samprofessional@gmail.com>
2015-03-07 17:23:29 -0800

    Make sure to get posts made by person

--- services/facebook.js
+++ services/facebook.js
@@ -13,12 +13,20 @@ var facebook = {
   },
 
   getStatuses: function(options) {
+    options = options || {};
+
+    var profileId = options.PROFILE_ID || process.env.PROFILE_ID;
+
     return new Promise(function(resolve, reject) {
-        this.graph.get(process.env.PROFILE_ID + "?fields=feed", function(err, graphRes) {
+        this.graph.get(profileId + "?fields=feed", function(err, graphRes) {
           if (err) { reject(err); }
 
           var data = graphRes.feed.data;
-          var statuses = data.map(function(status) {
+          var statuses = data.filter(function(post) {
+            return post.message && post.from.id === profileId;
+          });
+
+          statuses = statuses.map(function(status) {
             return _.pick(status, 'message', 'created_time');
           });
 
